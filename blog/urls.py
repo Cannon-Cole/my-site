7817f7e453c52cc9326e5a9fda8ca0e290b39d1f
Cannon-Cole <colecannonschool@gmail.com>
2022-04-07 00:21:19 -0600

    Added links to navigation and all posts to specific post. Added placeholder post. FIxed path name typo

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.starting_page, name="starting_page"),
+    path('', views.starting_page, name="starting-page"),
     path('posts', views.posts, name="posts"),
     path('posts/<slug:post>', views.specific_post, name="specific-post")
 ]
