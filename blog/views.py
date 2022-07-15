ab05f45deb058f103c20f1ecde99b3734db8e91d
Cannon-Cole <colecannonschool@gmail.com>
2022-07-15 01:01:38 -0600

    Added tags to views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from datetime import date
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from blog.models import Post
 
 all_posts = [
     {"slug": "hike-in-the-mountains",
@@ -76,22 +78,22 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 
 def specific_post(request, post):
-    identified_post = next(
-        specified for specified in all_posts if specified['slug'] == post)
+    identified_post = get_object_or_404(Post, slug=post)
     return render(request, "blog/specific-post.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
